d51cb2a59b5c85a0fc5e1e14614bf7e4600fbf48
Ruben Lopez <rjlopez2@gmail.com>
2021-12-30 20:43:19 +0100

    Allow automatic lounch of the app in brawser and usefull traceback info

--- shiny-run.R
+++ shiny-run.R
@@ -1,3 +1,8 @@
 # this iscript is used to run the application in file defined in app.r in the background
-options(shiny.autoreload = T)
+options(shiny.autoreload = T,
+        shiny.trace=T,
+        # shiny.port = 6133,
+        shiny.launch.browser = T)
+
+
 shiny::runApp()
